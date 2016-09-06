290ac388038c87b921b493c155db054b1c1ff2ae
whu-iss-jack <1565771886@qq.com>
2016-09-06 10:36:36 +0800

    他妈的，不知道这个是怎么搞得

--- app/src/main/java/com/jinhanyu/jack/langren/MainActivity.java
+++ app/src/main/java/com/jinhanyu/jack/langren/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URISyntaxException;
